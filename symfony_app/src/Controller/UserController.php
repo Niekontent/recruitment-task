c83ed70d21d88ca07db3853316b7f958579ef70f
Maciek <zaambarasowany@gmail.com>
2026-02-16 18:17:42 +0100

    Remove symfony_app as submodule, add it again, wipe tears and stop crying

---
+++ symfony_app/src/Controller/UserController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Controller;
+
+use App\Service\PhoenixApiClient;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Psr\Log\LoggerInterface;
+
+class UserController extends AbstractController
+{
+    private PhoenixApiClient $api;
+
+    public function __construct(PhoenixApiClient $api)
+    {
+        $this->api = $api;
+    }
+    public function index(Request $request): Response
+    {
+        // filtrowanie i sortowanie
+        $filters = [
+            'first_name' => $request->query->get('first_name'),
+            'last_name'  => $request->query->get('last_name'),
+            'gender'     => $request->query->get('gender'),
+            'birthdate_from' => $request->query->get('birthdate_from'),
+            'birthdate_to'   => $request->query->get('birthdate_to'),
+            'sort_by'    => $request->query->get('sort_by', 'id'),
+            'sort_order' => $request->query->get('sort_order', 'asc')
+        ];
+
+        $users = $this->api->listUsers($filters);
+
+        return $this->render('user/index.html.twig', [
+            'users' => $users,
+            'filters' => $filters
+        ]);
+    }
+    public function new(): Response
+    {
+        return $this->render('user/form.html.twig', [
+            'action' => 'Create',
+            'user' => [],
+        ]);
+    }
+
+    public function create(Request $request, LoggerInterface $logger): JsonResponse
+    {
+        $data = $request->request->all();
+        $logger->info('Create user data:', $data);
+
+        try {
+            $user = $this->api->createUser($data);
+            return new JsonResponse($user, 201);
+        } catch (\Exception $e) {
+            return new JsonResponse(['error' => $e->getMessage()], 500);
+        }
+        die();
+    }
+    
+    public function edit(int $id): Response
+    {
+        $user = $this->api->getUser($id);
+
+        return $this->render('user/form.html.twig', [
+            'user' => $user,
+            'action' => "Update"
+        ]);
+    }
+
+    public function update(int $id, Request $request): JsonResponse
+    {
+        $data = $request->request->all();
+
+        try {
+            $user = $this->api->updateUser($id, $data);
+            return new JsonResponse($user);
+        } catch (\Exception $e) {
+            return new JsonResponse(['error' => $e->getMessage()], 500);
+        }
+    }
+
+    public function delete(int $id): Response
+    {
+        try {
+            $this->api->deleteUser($id);
+        } catch (\Throwable $e) {
+            $this->addFlash('error', 'Failed to delete user');
+        }
+
+        return $this->redirectToRoute('user_index');
+    }
+
+}
---
+++ symfony_app/src/Service/PhoenixApiClient.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+
+class PhoenixApiClient
+{
+    private HttpClientInterface $client;
+    private string $baseUrl;
+    private ?string $apiToken;
+
+    public function __construct(HttpClientInterface $client, string $baseUrl, ?string $apiToken = null)
+    {
+        $this->client = $client;
+        $this->baseUrl = rtrim($baseUrl, '/');
+        $this->apiToken = $apiToken;
+    }
+
+    private function request(string $method, string $url, array $options = []): array
+    {
+        $headers = $options['headers'] ?? [];
+
+        if ($this->apiToken) {
+            $headers['Authorization'] = $this->apiToken;
+        }
+
+        $options['headers'] = $headers;
+
+        try {
+            $response = $this->client->request($method, $this->baseUrl . $url, $options);
+            return $response->toArray();
+        } catch (TransportExceptionInterface|ClientExceptionInterface|ServerExceptionInterface $e) {
+            return [];
+        }
+    }
+
+    // ---------- USERS ----------
+
+    public function listUsers(array $filters = []): array
+    {
+        $query = http_build_query($filters);
+        $url = '/api/users' . ($query ? "?$query" : '');
+        return $this->request('GET', $url);
+    }
+
+    public function getUser(int $id): array
+    {
+        return $this->request('GET', "/api/users/{$id}");
+    }
+
+    public function createUser(array $data): array
+    {
+        return $this->client->request('POST', $this->baseUrl . '/api/users', [
+            'json' => $data
+        ])->toArray();
+    }
+
+    public function updateUser(int $id, array $data): array
+    {
+        return $this->client->request('PUT', $this->baseUrl . '/api/users/' . $id, [
+            'json' => $data
+        ])->toArray();
+    }
+
+    public function deleteUser(int $id): void
+    {
+        $this->request('DELETE', '/api/users/' . $id);
+    }
+}
